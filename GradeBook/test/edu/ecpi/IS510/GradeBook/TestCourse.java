02710a254e0b640e740c813a05e74d1b54edddb9
kbutler0954 <kbutler1@gmail.com>
2011-09-21 21:02:40 -0400

    Added JUnit testing however no testing is implemented.

---
+++ GradeBook/test/edu/ecpi/IS510/GradeBook/TestCourse.java
@@ -0,0 +1,84 @@
+package edu.ecpi.IS510.GradeBook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCourse {
+
+	@Test
+	public void testCourseStringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCourse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAssignment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFinalGrade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFinalGrade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCredits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCredits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateFinalGrades() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateFinalGradesLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCourseNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCourseNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ GradeBook/test/edu/ecpi/IS510/GradeBook/TestMain.java
@@ -0,0 +1,14 @@
+package edu.ecpi.IS510.GradeBook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
